e1a272a19cbcfe3d2ee6835a84cb3d95125d3c9d
Vitali B <vitaliyborichev@gmail.com>
2020-03-15 16:29:44 +0300

    add pagination. finish

---
+++ src/Components/Spinner/Spinner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Spinner.css"
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-disk-01yri0g3ecrw">
+      <div className="ldio-tfjix1ikgn">
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Spinner
